13abe25ff967c0d5e49e3abe429468d94782def5
onei2l <onei2l@proton.me>
2022-12-03 15:14:47 +0100

    day 03

---
+++ 03/index.php
@@ -0,0 +1,39 @@
+<?php
+
+$lines = explode("\n", file_get_contents('input.txt'));
+
+$alphabet = array_merge(range('a', 'z'), range('A', 'Z'));
+
+$result = 0;
+foreach ($lines as $line) {
+	$line_split_array = mb_str_split($line, strlen($line) / 2);
+
+	foreach (mb_str_split($line_split_array[0]) as $character) {
+		if(strrpos($line_split_array[1], $character) !== false) {
+			$result += array_search($character, $alphabet) + 1;
+
+			break;
+		}
+	}
+}
+var_dump($result);
+
+// B
+
+$result_b = 0;
+$i = 1;
+$a = [];
+foreach ($lines as $line) {
+	$a = array_merge($a, [$line]);
+
+	if (!($i % 3)) {
+		$unique = array_intersect(array_unique(mb_str_split($a[0])), array_unique(mb_str_split($a[1])), array_unique(mb_str_split($a[2])));
+
+		$result_b += array_search(reset($unique), $alphabet) + 1;
+
+		$a = [];
+	}
+
+	$i++;;
+}
+var_dump($result_b);
