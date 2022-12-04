1d4a1d1a381ab3ffcb0a464c3a21cbb0d6c735e1
onei2l <onei2l@proton.me>
2022-12-04 11:30:20 +0100

    day 04

---
+++ 04/index.php
@@ -0,0 +1,33 @@
+<?php
+
+$lines = explode("\n", file_get_contents('input.txt'));
+
+$overlap_unique = $overlap_total= 0;
+foreach ($lines as $line) {
+	list($first_pair, $second_pair) = explode(',', $line);
+
+	list($first_section_first_number, $first_section_second_number) = explode('-', $first_pair);
+	list($second_section_first_number, $second_section_second_number) = explode('-', $second_pair);
+
+	if (
+		($first_section_first_number <= $second_section_first_number)
+		&& ($second_section_second_number <= $first_section_second_number)
+		|| ($second_section_first_number <= $first_section_first_number)
+		&& ($first_section_second_number <= $second_section_second_number)
+	) {
+		$overlap_unique++;
+	}
+
+	// B
+
+	$arf = range($first_section_first_number, $first_section_second_number);
+	$ass = range($second_section_first_number, $second_section_second_number);
+
+	if (array_intersect($arf, $ass)) {
+		$overlap_total++;
+	}
+}
+
+var_dump($overlap_unique);
+
+var_dump($overlap_total);
